9e80a82e13ab891d5327270c654f6d14bac898ee
Seth Louis <sethvihanlouis@gmail.com>
2023-10-31 03:40:23 -0400

    ad

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,7 +1,11 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.8
-const ENGINE_POWER = 300
+const ENGINE_POWER = 200
+var engine_accel = 0
+
+@onready var pivot = $Pivot
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +13,21 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	steering = move_toward(steering, Input.get_axis("ui_right","ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("ui_up","ui_down") * ENGINE_POWER
-	print(engine_force)
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("ui_right","ui_left") * MAX_STEER, delta)
+	engine_force = Input.get_axis("ui_up","ui_down") * (ENGINE_POWER + engine_accel)
+	pivot.global_position = pivot.global_position.lerp(global_position,delta * 20)
+	
+	if Input.is_action_pressed("ui_up"):
+		engine_accel += 5
+		if engine_accel == 300:
+			engine_accel = 300
+	else:
+		engine_accel = 0
+	
+	if Input.is_action_pressed("ui_down"):
+		$BackCamera3D.current = true
+		$Pivot/FrontCamera3D.current = false
+	else:
+		$BackCamera3D.current = false
+		$Pivot/FrontCamera3D.current = true
